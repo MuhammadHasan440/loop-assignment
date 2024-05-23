2ed43495a2dae9d25715f9819d6ca90b925f08ee
Muhammad Hasan <153660528+MuhammadHasan440@users.noreply.github.com>
2024-05-23 05:21:32 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,48 @@
+// Answer # 1 :
+// for(let i = 1; i <= 5; i++){
+//     console.log('hello world' , i);
+// }
+
+// Answer # 2 :
+//  for(let i = 1; i <= 10; i++){
+//         console.log(i);
+//     }
+
+// Answer # 3 :
+// const userInput = +prompt('Enter a number to print its multiplication table')
+// const times = +prompt('Enter length of multiplication table')
+// for (let i = 1; i <= times; i++) {
+//     console.log(`${userInput} * ${i} = ${userInput*i}`);
+// }
+
+// Answer # 4 :
+// let mobiles = ['Nokia' , 'Samsung' , 'Apple' , 'Sony' , 'Hwawei'];
+// const ul = document.querySelector('ul');
+// for (let i = 0; i < mobiles.length; i++) {
+//     console.log(mobiles[i]);
+//     ul.innerHTML += `<li>${mobiles[i]}</li>`
+// }
+
+// Answer # 5 :
+// let fruits = ['Apple' , 'Banana' , 'Orange' , 'Mango' , 'Strawberry']
+// const ul = document.querySelector('ul')
+// for (let i = 0; i < fruits.length; i++) {
+//     console.log(fruits[i] , i);
+//    ul.innerHTML += `<li>${fruits[i]}</li>`
+//    ul.innerHTML += `<li>${fruits[i] , i}</li>`
+// }
+
+// Answer # 6 :
+// const N = parseInt(prompt("Enter number of items:"));
+// let items = [];
+// for (let i = 0; i < N; i++) {
+//     let item = prompt("Enter value of index " + (i + 1) + ":");
+//     items.push(item);
+// }
+// document.write("Number of items:", N);
+// document.write(" items:", items);
+
+// Answer # 7 :
+
+
+
